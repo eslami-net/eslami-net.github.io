703dc94f0280c13a6b91b98f02f89a48b408bce0
hamed eslami <dr.hamed.eslami@gmail.com>
2022-01-07 18:23:49 +0330

    grid updated

--- src/components/parts/profession/Project.jsx
+++ src/components/parts/profession/Project.jsx
@@ -19,52 +19,55 @@ export default function Project({ projects }) {
       >
         Projects
       </Typography>
-      <Grid
-        container
-        spacing={2}
+
+      <div
         style={{
-          padding: 30,
+          display: "flex",
+          flexDirection: matches ? "row" : "column",
+          justifyContent: "center",
+          alignContent: "center",
         }}
       >
         {projects?.map((p) => {
           return (
-            <Grid item xs={12} md={4}>
-              <Card
-                style={{
-                  borderRadius: 25,
-                }}
-              >
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="240"
-                    image={p?.images[0]?.path}
-                    // alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {p.title}
-                    </Typography>
-                    <Typography
-                      height="140"
-                      noWrap
-                      variant="body2"
-                      color="text.secondary"
-                    >
-                      {p.description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  {/* <Button size="small" color="primary">
+            <Card
+              style={{
+                borderRadius: 10,
+                margin: 10,
+                padding: 10,
+                width: matches ? "400px" : "90%",
+              }}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="240"
+                  image={p?.images[0]?.path}
+                  // alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {p.title}
+                  </Typography>
+                  <Typography
+                    height="140"
+                    noWrap
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {p.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                {/* <Button size="small" color="primary">
                     Open
                   </Button> */}
-                </CardActions>
-              </Card>
-            </Grid>
+              </CardActions>
+            </Card>
           );
         })}
-      </Grid>
+      </div>
     </>
   );
 }
