68919b9f5d983b21196c8a71441b19aec8dd34b6
hamed eslami <dr.hamed.eslami@gmail.com>
2022-01-07 17:32:07 +0330

    projects deployed

--- src/components/parts/profession/Project.jsx
+++ src/components/parts/profession/Project.jsx
@@ -4,65 +4,67 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 import useMediaQuery from "@mui/material/useMediaQuery";
 export default function Project({ projects }) {
   const matches = useMediaQuery("(min-width:600px)"); // is greater than 600
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: matches ? "row" : "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-        // marginTop: 20,
-      }}
-    >
-      {projects?.map((p) => {
-        return (
-          <Card
-            sx={{ minWidth: 345 }}
-            // style={{
-            //   margin: 10,
-            // }}
-            style={{
-              borderRadius: 25,
-              // width: matches ? "20%" : "90%",
-              // minHeight: "200px",
-              // minWidth: "220px",
-              // marginTop: matches ? 0 : 20,
-            }}
-          >
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="240"
-                image={p?.images[0]?.path}
-                // alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {p.title}
-                </Typography>
-                <Typography
-                  height="140"
-                  noWrap
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  {p.description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Open
-              </Button>
-            </CardActions>
-          </Card>
-        );
-      })}
-    </div>
+    <>
+      <Typography
+        variant="h5"
+        style={{
+          padding: 30,
+        }}
+      >
+        Projects
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        style={{
+          padding: 30,
+        }}
+      >
+        {projects?.map((p) => {
+          return (
+            <Grid item xs={12} md={4}>
+              <Card
+                style={{
+                  borderRadius: 25,
+                }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="240"
+                    image={p?.images[0]?.path}
+                    // alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {p.title}
+                    </Typography>
+                    <Typography
+                      height="140"
+                      noWrap
+                      variant="body2"
+                      color="text.secondary"
+                    >
+                      {p.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  {/* <Button size="small" color="primary">
+                    Open
+                  </Button> */}
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
   );
 }
